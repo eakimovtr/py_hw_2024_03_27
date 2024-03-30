ee294912b9763ae3ff909bae61f4437fe79188d9
Egor Akimov <akimoved@gmail.com>
2024-03-30 23:56:46 +0400

    fix comments for task 2

--- academic_performance.py
+++ academic_performance.py
@@ -10,6 +10,7 @@ class Student:
     def get_marks(self) -> list[int]:
         return self.marks
     
+    # check if student can apply for scholarship
     def is_eligible_scholarship(self) -> bool:
         return sum(self.marks) / len(self.marks) >= 10.7
     
@@ -25,19 +26,20 @@ class Student:
             raise ValueError("Invalid value for mark")
         self.marks[key] = value
      
+    # Method for user input of marks list
     @staticmethod
     def input_marks() -> list[int]:
         print("Enter marks separated by comma (,)")
         usr_input: list = input().strip().split(",")
-        if len(usr_input) != 10:
+        if len(usr_input) != 10: # check whether entered list is of correct length 10
             raise ValueError("Number of marks must be 10")
         res: list[int] = []
         for str_mark in usr_input:
             try:
-                mark = int(str_mark)
+                mark = int(str_mark) # typecast string value of mark to int
             except ValueError as e:
-                mark = round(mark)
-            if not Student.check_mark_value(mark):
+                mark = round(mark) # try to typecast string value by rounding if it happens to be float
+            if not Student.check_mark_value(mark): # check for correct value of mark
                 raise ValueError("Invalid value for mark")
             res.append(mark)
         return res
